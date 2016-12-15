7a386c132c8618f07664217bc9bfab1baa7e2367
nathanjessen <nathan.jessen@gmail.com>
2016-12-14 21:44:46 -0800

    Update gulp and its packages. Clean up assets
    Update dev dependencies and update gulp file to fix errors with running
    jekyll and gulp at the same time. Gulp will now serve local site using
    child process.
    Reduce number of assets by concatenating several files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,104 @@
-var gulp = require('gulp'),
-  gutil = require('gulp-util'),
-  sass = require('gulp-ruby-sass'),
-  prefix = require('gulp-autoprefixer'),
-  minifyCSS = require('gulp-minify-css'),
-  uncss = require('gulp-uncss'),
-  livereload = require('gulp-livereload'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify')
-  imagemin = require('gulp-imagemin'),
-  glob = require('glob'),
-  rename = require('gulp-rename'),
-  notify = require("gulp-notify");
+const child = require('child_process');
+const browserSync = require('browser-sync').create();
 
-gulp.task('styles', function () {
-  gulp.src('_site/css/main.css')
-    .pipe(prefix("> 5%"))
-    .pipe(minifyCSS())
+const gulp = require('gulp');
+const prefix = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+const cleanCSS = require('gulp-clean-css');
+const notify = require('gulp-notify');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const gutil = require('gulp-util');
+
+const siteRoot = '_site';
+const sassFiles = '_scss/**/*.scss';
+const jsFiles = '_assets/js/*.js';
+const imageFiles = '_assets/img/**/*.{jpg,png,gif}';
+
+// Sass
+gulp.task('sass', function () {
+  gulp.src(sassFiles)
+    .pipe(sass())
+    .pipe(prefix('> 5%'))
+    .pipe(cleanCSS())
     .pipe(rename('main.min.css'))
-    .pipe(notify("css minified"))
-    .pipe(gulp.dest('css'));
-
-  gulp.src([
-      '_assets/vendors/reveal/css/reveal.css',
-      '_assets/vendors/reveal/css/theme/beige.css'
-    ])
-    .pipe(minifyCSS())
+    .pipe(notify('css optimized'))
     .pipe(gulp.dest('assets/css'));
 });
 
-
+// JavaScript
 gulp.task('scripts', function () {
-  gulp.src([
-      '_assets/js/main.js'
-    ])
-    .pipe(concat("main.min.js"))
+  gulp.src(jsFiles)
+    .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('assets/js'));
+});
 
+// Vendor JS
+gulp.task('vendor-js', function () {
   gulp.src([
       '_assets/vendors/reveal/js/reveal.js'
     ])
-    .pipe(concat("reveal.min.js"))
+    .pipe(concat('reveal.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('assets/js'));
+});
 
+// Vendor CSS
+gulp.task('vendor-css', function () {
+  gulp.src([
+    '_assets/vendors/reveal/css/reveal.css',
+    '_assets/vendors/reveal/css/theme/beige.css'
+  ])
+  .pipe(concat('reveal.beige.css'))
+  .pipe(cleanCSS())
+  .pipe(gulp.dest('assets/css'));
 });
 
 // Images
 gulp.task('imagemin', function () {
-  gulp.src('_assets/img/**/*.{jpg,png,gif}')
+  gulp.src(imageFiles)
   .pipe(imagemin({
     optimizationLevel: 7
   }))
   .pipe(gulp.dest('assets/img'));
 });
 
-gulp.task('watch', function () {
-  var server = livereload();
-  gulp.watch('_site/css/main.css', ['styles']);
-  gulp.watch('_assets/js/*.js', ['scripts']);
-  gulp.watch('_assets/img/**', ['imagemin']);
-  // gulp.watch('_assets/css/**').on('change', function(file) {
-  // 	server.changed(file.path);
-  // });
+// Jekyll
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['serve',
+    '--watch',
+    '--incremental',
+    '--drafts'
+  ]);
+
+  const jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
 });
 
+// BrowserSync
+gulp.task('serve', () => {
+  browserSync.init({
+    files: [siteRoot + '/**'],
+    port: 4000,
+    server: {
+      baseDir: siteRoot
+    }
+  });
 
-gulp.task('default', ['watch']);
+  // Watch
+  gulp.watch(sassFiles, ['sass']);
+  gulp.watch(jsFiles, ['scripts']);
+  gulp.watch(imageFiles, ['imagemin']);
+});
+
+// Default
+gulp.task('default', ['vendor-css', 'vendor-js', 'scripts', 'sass', 'jekyll', 'serve']);
